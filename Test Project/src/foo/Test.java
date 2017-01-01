6ba4cd3cf97ffa6e3158ef4ac3f72f35b5ccb02a
Fubarable <pete_stein@yahoo.com>
2017-01-01 15:49:07 -0500

    Big change to code!

--- Test Project/src/foo/Test.java
+++ Test Project/src/foo/Test.java
@@ -3,5 +3,6 @@ package foo;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("WTF, let's add another line!");
     }
 }
